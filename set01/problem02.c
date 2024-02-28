b57b9ae5d74343999981d9f7bc2ad3fb24214150
geethika.singh20@gmail.com <geethika.singh20@gmail.com>
2024-02-28 16:13:07 +0530

    Added a function to add numbers if the number of variables is varying

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,9 +1,10 @@
 //2. Write a C program to add two numbers.
 #include <stdio.h>
 #include<stdlib.h>
+//#include <stdarg.h>
 
 
-
+//function to read numbers
 void readNumbers(float *num1,float *num2){
     printf("Enter the first number to be added");
     if(!scanf("%f",num1))
@@ -18,10 +19,35 @@ void readNumbers(float *num1,float *num2){
           exit(1);
         }       
 }
+
+//function to add numbers
 float addNumbers(float num1,float num2){
     return num1+num2;   
 }
 
+/*
+//function to add varying number of inputs
+float addVarNumbers(int num,...){
+    printf("inside var fn");
+    int a;
+    float sum=0;
+    va_list args;
+
+    va_start(args, num);
+
+    for (int i = 0; i < num; i++)
+    {
+        sum=sum+(va_arg(args,int));
+    }
+
+    printf("Sum %f",sum);
+
+    va_end(args);
+
+}
+*/
+
+//function to display the result
 void displayResult(float result)
 {
     printf("The result is %f\n",result);
@@ -32,5 +58,8 @@ int main()
 float num1,num2;
 readNumbers(&num1,&num2);
 displayResult(addNumbers(num1,num2));
+
+//addVarNumbers(3,4,2,8);
+
 return 0;
 }
